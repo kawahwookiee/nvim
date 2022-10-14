a8e92fced51c550150df19aeb52e2f886691bf9d
kawahwookiee <a.chtepine@gmail.com>
2022-10-14 11:54:13 +0400

    added gitignore

--- lua/plug.lua
+++ lua/plug.lua
@@ -1,5 +1,23 @@
 -- [[ plug.lua ]]
 
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('config')..'/site/pack/packer/start/packer.nvim'
+  print(install_path)
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+-- Update the packpath
+local packer_path = vim.fn.stdpath('config') .. '/site'
+vim.o.packpath = vim.o.packpath .. ',' .. packer_path
+
 return require('packer').startup(function(use)
   use { 'kyazdani42/nvim-tree.lua', requires = 'kyazdani42/nvim-web-devicons' }
   use { 'dracula/vim', as = 'dracula' }
@@ -19,4 +37,9 @@ return require('packer').startup(function(use)
   use { 'terrortylor/nvim-comment' }
   use { 'RRethy/vim-illuminate' }
   use { 'simrat39/rust-tools.nvim' }
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
--- lua/vars.lua
+++ lua/vars.lua
@@ -9,6 +9,4 @@ g.cpp_attributes_highlight = 1
 g.cpp_member_highlight = 1
 g.cpp_named_requirements_highlight = 1
 
--- Update the packpath
-local packer_path = vim.fn.stdpath('config') .. '/site'
-vim.o.packpath = vim.o.packpath .. ',' .. packer_path
+
