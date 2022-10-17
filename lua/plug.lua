f959f0bc64a7ec914535e840cce23c400cbc0453
kawahwookiee <a.chtepine@gmail.com>
2022-10-17 10:48:34 +0400

    upd

--- lua/plug.lua
+++ lua/plug.lua
@@ -2,11 +2,10 @@
 
 local ensure_packer = function()
   local fn = vim.fn
-  local install_path = fn.stdpath('config')..'/site/pack/packer/start/packer.nvim'
-  print(install_path)
+  local install_path = fn.stdpath('config') .. '/site/pack/packer/start/packer.nvim'
   if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
+    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+    vim.cmd("packadd packer.nvim")
     return true
   end
   return false
