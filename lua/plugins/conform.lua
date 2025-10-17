9795981ef3de345c9fe20d283970b8e43d4f7cd3
kawahwookiee <a.chtepine@gmail.com>
2025-10-17 14:23:25 +0200

    upd bashls

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -14,7 +14,9 @@ return {
     },
   },
   opts = {
-    -- LazyVim will use these options when formatting with the conform.nvim formatter
+    formatters = {
+      shfmt = { args = { "-filename", "$FILENAME", "-i", "4" } },
+    },
     default_format_opts = {
       timeout_ms = 3000,
       async = false, -- not recommended to change
