87206dbe287af6bde6ddedbb68c04bb68f8b66ac
kawahwookiee <a.chtepine@gmail.com>
2026-02-22 12:53:04 +0100

    upd blink

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,10 +3,22 @@ return {
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
+    fuzzy = { implementation = "prefer_rust_with_warning" },
     sources = {
-      compat = {},
       default = { "lsp", "path", "snippets", "buffer" },
+      per_filetype = {
+        lua = { inherit_defaults = true, "lazydev" },
+      },
+      providers = {
+        lazydev = {
+          name = "LazyDev",
+          module = "lazydev.integrations.blink",
+          score_offset = 100, -- show at a higher priority than lsp
+        },
+      },
+      compat = {},
     },
+    completion = { keyword = { range = "full" } },
     keymap = {
       preset = "enter",
       ["<Esc>"] = { "hide", "fallback" },
